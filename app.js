a8c9b7fed0cd0ec3d4eb92202ad2c14e7e0d6c72
ertdgu3579-rgb <ertdgu3579@gmail.com>
2026-06-09 00:35:01 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -10,3 +10,43 @@ buttons.forEach((button) => {
 
 });
 
+const year = document.getElementById("dateInput1");
+const month = document.getElementById("dateInput2");
+const day = document.getElementById("dateInput3");
+
+function checkInput() {
+
+    if (
+        year.value.length === 4 &&
+        month.value.length === 2 &&
+        day.value.length === 2
+    ) {
+
+        window.location.href = "time.html";
+
+    }
+
+}
+
+// 숫자만 허용 + 검사
+[year, month, day].forEach((input) => {
+
+    input.addEventListener("input", () => {
+
+        input.value = input.value.replace(/[^0-9]/g, '');
+
+        checkInput();
+
+    });
+
+});
+const submitBtn = document.getElementById("complete");
+const message = document.getElementById("message");
+
+submitBtn.onclick = function () {
+
+    message.readOnly = false;
+
+    message.focus();
+
+};
